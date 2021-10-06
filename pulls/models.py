c1d982e8cd43ccaa74cecebd7c2da44cf3a68cba
Roman373 <roman.izibaeff2017@yandex.ru>
2021-10-06 13:44:53 +0500

    <d>

--- pulls/models.py
+++ pulls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    login = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=50)
+    data = models.DateField()
+    status = models.CharField(max_length=20)
+    User_id = models.ForeignKey(User, on_delete=models.CASCADE)
